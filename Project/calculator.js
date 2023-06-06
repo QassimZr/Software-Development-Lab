34af80aeb41ce1f14261eb6b25b257a4d9b05b79
QassimZr <qassimalfarsy000@students.tukenya.ac.ke>
2023-06-07 00:08:20 +0300

    Updated the display to function
    display shows the answer
    added a calc logo

--- Project/calculator.js
+++ Project/calculator.js
@@ -1,22 +1,38 @@
-function appendCharacter(char) {
-    document.getElementById('display').value += char;
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function backspace() {
+  var displayValue = document.getElementById('display').value;
+  document.getElementById('display').value = displayValue.slice(0, -1);
+}
+
+function calculatePercentage() {
+  var displayValue = document.getElementById('display').value;
+  var result;
+
+  try {
+    result = parseFloat(displayValue) / 100;
+  } catch (error) {
+    result = 'Invalid expression';
   }
-  
-  function clearDisplay() {
-    document.getElementById('display').value = '';
+
+  document.getElementById('display').value = result;
+}
+
+function calculateResult() {
+  var displayValue = document.getElementById('display').value;
+  var result;
+
+  try {
+    result = eval(displayValue);
+  } catch (error) {
+    result = 'Invalid expression';
   }
-  
-  function deleteLastChar() {
-    var display = document.getElementById('display');
-    display.value = display.value.slice(0, -1);
-  }
-  
-  function evaluate() {
-    var display = document.getElementById('display');
-    var result = eval(display.value);
-    display.value = result;
-  
-    // Display result and clear the display for the next input
-    display.value = result;
-  }
-  
+
+  document.getElementById('display').value = result;
+}
